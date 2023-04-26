145a259d13b71433842f2db58069adbf2379e5a8
kamilogrodowczyk <kamil.web.development@gmail.com>
2023-04-27 00:30:45 +0200

    Create about/offer sections

--- front-page.php
+++ front-page.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
-HOME
+    <main>
+        <?php 
+            get_template_part('/components/home/about');
+            get_template_part('/components/home/offers');
+        ?>
+    </main>
 
 <?php get_footer(); ?>
