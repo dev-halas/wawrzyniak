80758ee827e92f6461b2b8ed2c365853c3c0ecb3
devkryu <dev.halas@gmail.com>
2023-06-20 21:56:31 +0200

    be refactor

---
+++ components/restaurant/events.php
@@ -0,0 +1,170 @@
+<section class="events-section" id="organizujemy">
+    <svg class="decoration" xmlns="http://www.w3.org/2000/svg" width="2364" height="1956.528" viewBox="0 0 2364 1956.528"><path d="M1201.328,277.006C1829.118,803.471,2364,529.2,2364,1182S1244.73,2482.829,937.688,1951.012,0,1834.8,0,1182,835.854-29.48,1201.328,277.006Z" transform="translate(0 -190.686)"/></svg>
+    <div class="container-big events-container" data-aos="zoom-in">
+        <h2>Organizujemy</h2>
+        <div class="events-elements">
+            <button class="events-button left-20 one">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button.png" alt="">
+                <span class="name">Przyjęcia</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button left-20 two">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-1.png" alt="">
+                <span class="name">Wesela</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-1-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button left-20 three">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-2.png" alt="">
+                <span class="name">Komunie</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-2-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button left-20 four">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button.png" alt="">
+                <span class="name">Imprezy plenerowe</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button left-20 five">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-long-button.png" alt="">
+                <span class="name">Imprezy okolicznościowe</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-long-button-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button right-20 one">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button.png" alt="">
+                <span class="name">Biznes</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button right-20 two">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-1.png" alt="">
+                <span class="name">Szkolenia</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-1-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button right-20 three">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-2.png" alt="">
+                <span class="name">Konferencje</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-small-button-2-hover.png" alt="">
+                </div>
+            </button>
+            <button class="events-button right-20 four">
+                <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button.png" alt="">
+                <span class="name">Imprezy firmowe</span>
+
+                <div class="events-button child">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/events-medium-button-hover.png" alt="">
+                </div>
+            </button>
+            <div class="events-element-container">
+                <h3>Przyjęcia</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Wesela</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Komunie</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Imprezy plenerowe</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Imprezy okolicznościowe</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Biznes</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Szkolenia</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Konferencje</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+            <div class="events-element-container">
+                <h3>Imprezy firmowe</h3>
+                <div class="events-element-text">
+                    <p>W życiu każdego z nas jest wiele chwil, które chcielibyśmy spędzić w otoczeniu najbliższych osób. 
+                        Zachęcamy Państwa do skorzystania z naszej oferty przyjęć okolicznościowych, ponieważ według nas każda, nawet najmniejsza uroczystość, jest szczególna.</p>
+
+                    <p>Profesjonalnie (i z pomysłem) pomożemy Państwu uczcić wyjątkowe okazje.</p>
+                </div>
+                <button class="close-btn"></button>
+            </div>
+        </div>
+    </div>
+</section>
--- components/restaurant/menu.php
+++ components/restaurant/menu.php
@@ -1,12 +1,16 @@
+<?php 
+    $menu_1 = get_field('menu_1');
+    $menu_2 = get_field('menu_2');
+?>
 <section id="menu-container">
     <div class="container menu-container">
         <h2 class="menu-header">Nasze menu</h2>
         <div id="lightGallery" class="menu-images">
             <div class="menu-image" data-aos="zoom-in" data-src="<?php echo THEME_URL; ?>_dev/img/menu-left.jpg">
-                <img src="<?php echo THEME_URL; ?>_dev/img/menu-left.jpg" alt="menu">
+                <img src="<?php echo esc_url($menu_1['url']); ?>" alt="menu">
             </div>
             <div class="menu-image" data-aos="zoom-in" data-src="<?php echo THEME_URL; ?>_dev/img/menu-right.jpg">
-                <img src="<?php echo THEME_URL; ?>_dev/img/menu-right.jpg" alt="menu">
+                <img src="<?php echo esc_url($menu_2['url']); ?>" alt="menu">
             </div>
         </div>
     </div>
